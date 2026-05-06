2d37871bc7333f66aaf8b5831918be10f07d8e3c
Abdu <abdu.nazar145@gmail.com>
2026-05-06 17:06:10 +0200

    feat: add pdf reader

--- pdf.ts
+++ pdf.ts
@@ -1,14 +1,13 @@
-import fs from "fs";
-import pdf from "pdf-parse";
+import fs from "node:fs";
+import { PDFParse } from "pdf-parse";
 
-async function pdfReader(){
-    const dataBuffer = fs.readFileSync("pdf.pdf")
-
-    const data = await pdf(dataBuffer);
-
-    console.log("Text:");
-    console.log(data.text);
-    
+async function pdfReader() {
+  const dataBuffer = fs.readFileSync("pdf.pdf");
+  const parser = new PDFParse({ data: dataBuffer });
+  const data = await parser.getText();
+  console.log("Text:");
+  console.log(data.text);
+  await parser.destroy();
 }
 
 pdfReader();
