21d88e7fa01bd1721b6d42599cfa06bbbe696e34
Elias-Larsson <aroezi05@gmail.com>
2026-05-06 17:40:10 +0200

    structured output for AI

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import "dotenv/config";
 import OpenAI from "openai";
+import { z } from "zod";
+import { zodTextFormat } from "openai/helpers/zod";
 
 const apiKey = process.env.OPENAI_API_KEY;
+
+const AI_output = z.object({
+  correct: z.boolean(),
+  grade: z.enum(["F", "E", "D", "C", "B", "A"]),
+});
 
 async function main(): Promise<void> {
   const client = new OpenAI({
@@ -10,7 +17,15 @@ async function main(): Promise<void> {
 
   const response = await client.responses.create({
     model: "gpt-4.1",
-    input: "Write a one-sentence bedtime story about a unicorn.",
+    input: [
+      {
+        role: "system",
+        content: "The earth is flat because the moon is tiny",
+      },
+    ],
+    text: {
+      format: zodTextFormat(AI_output, "AI_output"),
+    },
   });
 
   console.log(response.output_text);
