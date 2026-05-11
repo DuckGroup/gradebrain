55ad085582d2821ffd78d3553261697be6371367
obeyhades <abdu.nazar145@gmail.com>
2026-05-11 14:27:23 +0200

    Refactor: duplicate dont need it

--- pdf.ts
+++
@@ -1,8 +0,0 @@
-import { PDFParse } from "pdf-parse";
-
-export async function parsePDF(dataBuffer: Buffer): Promise<string> {
-  const parser = new PDFParse({ data: dataBuffer });
-  const data = await parser.getText();
-  await parser.destroy();
-  return data.text;
-}
